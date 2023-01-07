7c2773c78b38145904b652fca7e25edc74fcc3fc
Beaskyy <afeezakintobi@nqb8.co>
2023-01-07 23:02:52 +0100

    quiz-app

---
+++ 2/script.js
@@ -0,0 +1,42 @@
+const quizData = [
+  {
+    question: "How old is Florin",
+    a: "10",
+    b: "17",
+    c: "26",
+    d: "110",
+    correct: "c",
+  },
+  {
+    question: "What is the most used programming language in 2019",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d",
+  },
+  {
+    question: "Who is the president of US",
+    a: "Florin Pop",
+    b: "Joe Biden",
+    c: "Donald Trump",
+    d: "Barrack Obama",
+    correct: "b",
+  },
+  {
+    question: "What does HTML stands for",
+    a: "Hyper Text Markup Language",
+    b: "Hyper Texting Markup Language",
+    c: "Hypertext Markup Language",
+    d: "HyperTexting Markup Language",
+    correct: "c",
+  },
+  {
+    question: "What year was JavaSrcipt launched",
+    a: "1996",
+    b: "1997",
+    c: "1998",
+    d: "none of the above",
+    correct: c,
+  },
+];
